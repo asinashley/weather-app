f90b10224d816bc98534b644b5e90c214a752e83
Ashley Mungu <asinashley@gmail.com>
2024-04-04 14:36:14 -0900

    more weather data

--- index.js
+++ index.js
@@ -2,10 +2,42 @@ function getWeather(response) {
   let temperatureElemet = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind");
+  let time = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   city.innerHTML = response.data.city;
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+  time.innerHTML = formatDate(date);
   temperatureElemet = Math.round(temperature);
+
+  console.log(response.data.condition.description);
 }
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function searchCity(city) {
   let apiKey = "9ftcc7o8a93343aab7a08acbac525689";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
@@ -23,3 +55,5 @@ function citySearch(event) {
 
 let searchForm = document.querySelector("#search");
 searchForm.addEventListener("submit", citySearch);
+
+searchCity("Paris");
