dfe2489d0455e8598fb5a909716fdbf031119f01
Ashley Mungu <asinashley@gmail.com>
2024-04-04 13:43:20 -0900

    Added basic search
    No API

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function citySearch(event) {
   event.preventDefault();
-  let searchIput = document.querySelector("#searchIput");
+  let searchInput = document.querySelector("#searchInput");
   let city = document.querySelector("#city");
-  city.innerHTML = searchIput.value;
+  city.innerHTML = searchInput.value;
 }
 let searchForm = document.querySelector("#search");
 searchForm.addEventListener("submit", citySearch);
